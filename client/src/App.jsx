ec52a3a1944cf082811944bcb248bd48c0586475
victus <wwwranebrijesh254@gmail.com>
2023-12-14 21:56:44 +0530

    added comments for better understanding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,17 +8,28 @@ import Register from './pages/Register';
 const App = () => {
   return (
     <>
+      {/* BrowserRouter: Provides the context for the router to manage navigation */}
       <BrowserRouter>
+        {/* Routes: Container for defining different routes in the application */}
         <Routes>
+          {/* Route for the home page */}
           <Route path='/' element={<Home />} />
+          
+          {/* Route for the contact page */}
           <Route path='/contact' element={<Contact />} />
+          
+          {/* Route for the about page */}
           <Route path='/about' element={<About />} />
+          
+          {/* Route for the login page */}
           <Route path='/login' element={<Login />} />
+          
+          {/* Route for the register page */}
           <Route path='/register' element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
